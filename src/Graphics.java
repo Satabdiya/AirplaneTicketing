128338130226fbd9c89dfcbb3c442d843a15edbc
Satabdiya <roy127@purdue.edu>
2019-11-27 01:32:24 -0500

    Added the popUp window feature showing current passenger information

--- src/Graphics.java
+++ src/Graphics.java
@@ -21,6 +21,11 @@ public class Graphics {
                     frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
                     frame.setResizable(false);
                     panel = new JPanel();
+                    popUp = new JFrame();
+                    popUp.setResizable(false);
+                    popUp.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                    popUp.setSize(200, 200);
+                    encapsulate = new JPanel();
                     welcome();
                     frame.setVisible(true);
                 }
@@ -153,7 +158,7 @@ public class Graphics {
         JButton button1 = new JButton("Exit");
         JButton button2 = new JButton("Choose this flight");
         button1.addActionListener(e -> frame.dispose());
-        button2.addActionListener(e -> frame.setSize(300, 400));
+        button2.addActionListener(e -> confirmFlight((String) options.getSelectedItem()));
         buttons.add(button1);
         buttons.add(button2);
         panel.requestFocus();
@@ -195,12 +200,142 @@ public class Graphics {
         frame.add(panel);
     }
 
+    public static void confirmFlight(String flightName) {
+        panel.removeAll();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JPanel inner = new JPanel();
+        JLabel label = new JLabel();
+        label.setText("<html><center>Are you sure you want to book a flight on<br>" + flightName + " Airlines?" +
+                "</center></html>");
+        label.setFont(new Font(label.getFont().getName(), Font.BOLD, 15));
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
+        inner.add(label);
+        JPanel buttons = new JPanel();
+        buttons.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JButton button1 = new JButton("Exit");
+        JButton button2 = new JButton("No, I want a different flight.");
+        JButton button3 = new JButton("Yes, I want this flight.");
+        button1.addActionListener(e -> frame.dispose());
+        button2.addActionListener(e -> dropDownInfo());
+        button3.addActionListener(e -> passengerInfo(flightName));
+        buttons.add(button1);
+        buttons.add(button2);
+        buttons.add(button3);
+        panel.requestFocus();
+        panel.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent keyEvent) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+                if (keyEvent.getKeyCode() == KeyEvent.VK_SLASH) {
+                    passengerInfoServer(flightName);
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent keyEvent) {
+            }
+        });
+        panel.add(new JLabel("         "));
+        panel.add(new JLabel("         "));
+        panel.add(new JLabel("         "));
+        panel.add(new JLabel("         "));
+        panel.add(inner);
+        panel.add(new JLabel("         "));
+        panel.add(new JLabel("         "));
+        panel.add(new JLabel("         "));
+        panel.add(new JLabel("         "));
+        panel.add(new JLabel("         "));
+        panel.add(new JLabel("         "));
+        panel.add(new JLabel("         "));
+        panel.add(new JLabel("         "));
+        panel.add(new JLabel("         "));
+        panel.add(new JLabel("         "));
+        panel.add(buttons);
+        panel.updateUI();
+        panel.setVisible(true);
+        frame.add(panel);
+    }
+
+    public static void passengerInfo(String flightName){
+        panel.removeAll();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JPanel inner = new JPanel();
+        JLabel label = new JLabel();
+        label.setText("<html><center>Please input your information below.</center></html>");
+        label.setFont(new Font(label.getFont().getName(), Font.BOLD, 15));
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
+        inner.add(label);
+        JPanel questions = new JPanel();
+        questions.setLayout(new BoxLayout(questions, BoxLayout.PAGE_AXIS));
+        JLabel question1 = new JLabel("What is your first name?", SwingConstants.LEFT);
+        question1.setAlignmentX(Component.LEFT_ALIGNMENT);
+        JTextArea firstName = new JTextArea();
+        firstName.setRows(5);
+        firstName.setMaximumSize(new Dimension(frame.getWidth(), 150));
+        firstName.setEditable(true);
+        firstName.setAlignmentX(Component.LEFT_ALIGNMENT);
+        JLabel question2 = new JLabel("What is your last name?", SwingConstants.LEFT);
+        question2.setAlignmentX(Component.LEFT_ALIGNMENT);
+        JTextArea lastName = new JTextArea();
+        lastName.setRows(5);
+        lastName.setMaximumSize(new Dimension(frame.getWidth(), 150));
+        lastName.setEditable(true);
+        lastName.setAlignmentX(Component.LEFT_ALIGNMENT);
+        JLabel question3 = new JLabel("What is your age?", SwingConstants.LEFT);
+        question3.setAlignmentX(Component.LEFT_ALIGNMENT);
+        JTextArea age = new JTextArea();
+        age.setRows(5);
+        age.setMaximumSize(new Dimension(frame.getWidth(), 150));
+        age.setEditable(true);
+        age.setAlignmentX(Component.LEFT_ALIGNMENT);
+        questions.add(question1);
+        questions.add(firstName);
+        questions.add(new JLabel("   "));
+        questions.add(question2);
+        questions.add(lastName);
+        questions.add(new JLabel("   "));
+        questions.add(question3);
+        questions.add(age);
+        questions.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JPanel buttons = new JPanel();
+        buttons.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JButton button1 = new JButton("Exit");
+        JButton button2 = new JButton("Next");
+        button1.addActionListener(e -> frame.dispose());
+        button2.addActionListener(e -> frame.setSize(300, 300));
+        buttons.add(button1);
+        buttons.add(button2);
+        panel.requestFocus();
+        panel.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent keyEvent) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+                if (keyEvent.getKeyCode() == KeyEvent.VK_SLASH) {
+                    passengerInfoServer(flightName);
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent keyEvent) {
+            }
+        });
+        panel.add(new JLabel("         "));
+        panel.add(inner);
+        panel.add(questions);
+        panel.add(buttons);
+        panel.updateUI();
+        panel.setVisible(true);
+        frame.add(panel);
+    }
+
     public static void passengerInfoServer(String airline) {
-        popUp = new JFrame();
-        popUp.setResizable(false);
-        popUp.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        popUp.setSize(200, 200);
-        encapsulate = new JPanel();
+        encapsulate.removeAll();
         JPanel cp = (JPanel) popUp.getContentPane();
         ActionMap aMap = cp.getActionMap();
         InputMap inMap = cp.getInputMap();
@@ -246,11 +381,10 @@ public class Graphics {
         encapsulate.add(label);
         encapsulate.add(jsp);
         encapsulate.add(esc);
+        encapsulate.updateUI();
         encapsulate.setVisible(true);
         popUp.add(encapsulate);
+        popUp.setAlwaysOnTop(true);
         popUp.setVisible(true);
     }
-
-
-
 }
